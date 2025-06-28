ade160e4d634ebf622044307a1d3d2e96a343dca
jyhyun1008 <jayhyun1008@gmail.com>
2025-06-28 19:42:55 +0900

    Update run_server.sh

--- run_server.sh
+++ run_server.sh
@@ -1,1 +1,18 @@
-_common/_scripts/run_server.sh
+#!/bin/bash
+
+if [ -f _common/_config_common.yml ]; then
+    CFG=",_common/_config_common.yml"
+elif [ -f _config_common.yml ]; then
+    CFG=",_config_common.yml"
+fi
+
+INCREMENTAL="--incremental"
+if [ "$1" == "--full" ]; then
+    shift
+    INCREMENTAL=""
+fi
+
+# weird bug in jekyll
+rm -f _site/run_server.sh _site/_common/run_server.sh
+
+bundle exec jekyll serve --watch --config _config.yml${CFG} $INCREMENTAL "$@"
